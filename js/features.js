7be04415fc7c7e03aac883d3c6420d4f4698c277
Nure Hafsa <nurehafsa399@gmail.com>
2024-09-25 14:30:14 +0600

    add sticky

--- js/features.js
+++ js/features.js
@@ -9,14 +9,8 @@ document.getElementById('show-history-page')
         document.getElementById('show-history-page').style.background='#B4F461';
 
         showSectionById('history-section');
-        const now = new Date();
-            const formattedTime = now.toLocaleString(); // Format time as a readable string
-
-            // Update the donation history
-            const historyList = document.getElementById('historyList');
-            const newHistoryItem = document.createElement('li');
-            newHistoryItem.textContent = `Donated ${donationAmount} BDT on ${formattedTime}`;
-            historyList.appendChild(newHistoryItem);
+        
         
 
     });
+    
